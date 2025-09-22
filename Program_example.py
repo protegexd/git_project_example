303164d15348fe46c3c8a1f15f7daa1868cae429
User <example@example.com>
2025-09-22 17:05:57 +0500

    Дата

--- Program_example.py
+++ Program_example.py
@@ -1,5 +1,6 @@
 def main():
-    print('Hello my friend')
-    print('Why are you here?')
+    print('Danil Husniyarov')
+    print('ИВТ-ИСУ-202Б')
+    print('22.09.25')
 if __name__ =='__main__':
     main()
