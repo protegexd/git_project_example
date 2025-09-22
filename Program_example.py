6504e6db480bc47e204f72c56911a51c86ec6007
protegexd <xdprotege@gmail.com>
2025-09-22 17:21:39 +0500

    Update Program_example.py
    Я календарь, я календарь, я календарь
    Но почему, но почему

--- Program_example.py
+++ Program_example.py
@@ -2,5 +2,6 @@ def main():
     print('Danil Husniyarov')
     print('ИВТ-ИСУ-202Б')
     print('22.09.25')
+    print('Я календарь переверну И снова 3-е сентября')
 if __name__ =='__main__':
     main()
